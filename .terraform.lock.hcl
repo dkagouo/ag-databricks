895b967b56c1d5e980a7dc68a9f22239bc53e6e8
Aruna <aruna@mail>
2026-03-01 22:44:25 +1300

    Add Unity Catalog Metastore and Databricks Access Connector roles

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -1,6 +1,20 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
+provider "registry.terraform.io/databricks/databricks" {
+  version     = "1.110.0"
+  constraints = "~> 1.0"
+  hashes = [
+    "h1:QBjsl6Tjl1xJgpfM3vI1rcg2gbsR8j22zpCxbcMusSo=",
+    "zh:0e60d45c64471f0ec666b9cf639f0e390cef13eeba94b33b46404c6d71ea699b",
+    "zh:20149360d4c1157031caa25719dfa55fb121106599ea0b02ea59a6d8376f4f00",
+    "zh:47c94bdc998592072b5e53754270201b87f0d595b61f6ed8fd12bce62bcdc562",
+    "zh:7c96c03239e8c467f04bf7f11766248fb574d6511b9421110802464f93d52f98",
+    "zh:d6c5f6cbcc28949280b4f4d425d2c52d3d0ca9e186a95814fe96b7380ee00b57",
+    "zh:f95875da3c7caae77a5d7113a9a3d37672a39a10b0ba25e1a73fe53b3aab26b9",
+  ]
+}
+
 provider "registry.terraform.io/hashicorp/azurerm" {
   version     = "3.117.1"
   constraints = "~> 3.0"
--- main.tf
+++ main.tf
@@ -4,11 +4,20 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.0"
+    }
   }
 }
 
 provider "azurerm" {
   features {}
+}
+
+provider "databricks" {
+  host                        = azurerm_databricks_workspace.workspace.workspace_url
+  azure_workspace_resource_id = azurerm_databricks_workspace.workspace.id
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -42,4 +51,63 @@ resource "azurerm_storage_share" "fs" {
   name                 = "ag-fs"
   storage_account_name = azurerm_storage_account.sa.name
   quota                = 50
+}
+
+resource "azurerm_databricks_access_connector" "ext_access_connector" {
+  name                = "ext-access-connector"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_role_assignment" "ext_storage_role" {
+  scope                = azurerm_storage_account.sa.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_databricks_access_connector.ext_access_connector.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "storage_account_contributor" {
+  scope                = azurerm_storage_account.sa.id
+  role_definition_name = "Storage Account Contributor"
+  principal_id         = azurerm_databricks_access_connector.ext_access_connector.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "storage_queue_contributor" {
+  scope                = azurerm_storage_account.sa.id
+  role_definition_name = "Storage Queue Data Contributor"
+  principal_id         = azurerm_databricks_access_connector.ext_access_connector.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "eventgrid_contributor" {
+  scope                = azurerm_storage_account.sa.id
+  role_definition_name = "EventGrid EventSubscription Contributor"
+  principal_id         = azurerm_databricks_access_connector.ext_access_connector.identity[0].principal_id
+}
+
+resource "azurerm_storage_container" "metastore_container" {
+  name                  = "metastore-root"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
+
+resource "databricks_metastore" "metastore" {
+  name = "primary-metastore"
+  storage_root = format("abfss://%s@%s.dfs.core.windows.net/",
+    azurerm_storage_container.metastore_container.name,
+    azurerm_storage_account.sa.name)
+  force_destroy = true
+}
+
+resource "databricks_metastore_data_access" "metastore_data_access" {
+  metastore_id = databricks_metastore.metastore.id
+  name         = "mi_dac"
+  azure_managed_identity {
+    access_connector_id = azurerm_databricks_access_connector.ext_access_connector.id
+  }
+  is_default = true
+  depends_on = [
+    azurerm_role_assignment.ext_storage_role
+  ]
 }
